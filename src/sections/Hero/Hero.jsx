7c31438c466b6123000d5719689768d1a7e8b063
AAV2005 <156330619+AAV2005@users.noreply.github.com>
2024-09-02 19:26:56 +0530

    final_final

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from'./HeroStyles.module.css';
-import heroImg from '../../assets/Untitled design.png'
+import heroImg from '../../assets/final_final.png'
 import moon from '../../assets/moon.svg';
 import sun from '../../assets/sun.svg';
 import linkedinLight from '../../assets/linkedin-light.svg'
